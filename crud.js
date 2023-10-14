1b11b31ddeb369e32fa28bbaf4fe23c71f019faa
Evaldo Junior <evaaldormj@gmail.com>
2023-10-14 13:42:53 -0300

    Feat: conexão com o banco

---
+++ crud.js
@@ -0,0 +1,21 @@
+import { sql } from "./database.js";
+
+export class Crud {
+
+    create(atividade) {
+
+    }
+
+    read() {
+
+    }
+
+    update(idAtividade, atividade) {
+
+    }
+
+    delete(idDelete) {
+
+    }
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 import fastify from "fastify"
+import { Crud } from "./crud.js"
 
 const server = fastify({logger: true})
+
+// CREATE
+server.post('/todo', (req,res) => {
+    
+})
+
+// READ
+server.get('/todo', (req,res) => {
+    res.send('hello')
+})
+
+// UPTATE
+server.put('/todo/:id', (req,res) => {
+
+})
+
+// DELETE
+server.delete('/todo/:id', (req,res) => {
+
+})
 
 // Inicia o servidor
 server.listen({port: 3000}, () => console.log('Servidor iniciado!'))
