0e25a04e8dc1a3daa6a6b7bda37d5b6c32094931
Evaldo Junior <evaaldormj@gmail.com>
2023-10-14 14:21:17 -0300

    Feat: Finalização do CRUD e conexão com tabela

--- crud.js
+++ crud.js
@@ -2,20 +2,27 @@ import { sql } from "./database.js";
 
 export class Crud {
 
-    create(atividade) {
+    async create(task) {
+        const { id,atividade,status } = task
 
+        await sql`INSERT INTO todo (id,atividade,status) VALUES (${id},${atividade},${status})`
     }
 
-    read() {
+    async read() {
+        const tasks = await sql`SELECT * FROM todo`
 
+        return tasks
     }
 
-    update(idAtividade, atividade) {
+    async update(idTask, task) {
+        const idUp = idTask
+        const { id,atividade,status } = task
 
+        await sql`UPDATE todo SET id=${id}, atividade=${atividade}, status=${status} WHERE id=${idUp}`
     }
 
-    delete(idDelete) {
-
+    async delete(idDelete) {
+        await sql`DELETE FROM todo WHERE id=${idDelete}`
     }
 
 }
--- server.js
+++ server.js
@@ -1,27 +1,56 @@
 import fastify from "fastify"
 import { Crud } from "./crud.js"
 
-const server = fastify({logger: true})
+const server = fastify()
+const crud = new Crud()
 
 // CREATE
-server.post('/todo', (req,res) => {
-    
+server.post('/todo', async (req,res) => {
+    const { id,atividade,status } = req.body
+
+    await crud.create({
+        id,
+        atividade,
+        status
+    })
+
+    return res.send()
 })
 
 // READ
-server.get('/todo', (req,res) => {
-    res.send('hello')
+server.get('/todo', async (req,res) => {
+    const tasks = await crud.read()
+
+    return tasks
 })
 
 // UPTATE
-server.put('/todo/:id', (req,res) => {
+server.put('/todo/:id', async (req,res) => {
+    const idTask = req.params.id
+    const { id,atividade,status } = req.body
 
+    await crud.update(idTask, {
+        id,
+        atividade,
+        status
+    })
+
+    return res.send()
 })
 
 // DELETE
-server.delete('/todo/:id', (req,res) => {
+server.delete('/todo/:id', async (req,res) => {
+    const idDelete = req.params.id
 
+    await crud.delete(idDelete)
+
+    return res.send()
 })
 
 // Inicia o servidor
-server.listen({port: 3000}, () => console.log('Servidor iniciado!'))
+server.listen({
+    host: '0.0.0.0',
+    port: process.env.port ?? 3000
+}, () => {
+    console.log('Servidor iniciado!')
+})
